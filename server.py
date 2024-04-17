6e02dd08044ae898d2339b9ff51ea3aeea2c7201
Marc Chen <570171025@qq.com>
2024-04-17 09:35:50 +0800

    Update server.py

--- server.py
+++ server.py
@@ -450,6 +450,10 @@ class PromptServer():
         async def post_prompt(request):
             torch.cuda.empty_cache()
             logging.info("got prompt")
+            print("Is cuda avaliable in setup:", torch.cuda.is_available())
+            print("Cuda device count in setup:", torch.cuda.device_count())
+            print("cuda properties", torch.cuda.get_device_properties("cuda"))
+            
             resp_code = 200
             out_string = ""
             json_data =  await request.json()
