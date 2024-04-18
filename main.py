76af2d5e4bf0c8c8d1e1fc4b7d30cffe6d49b4a7
6174 <570171025@qq.com>
2024-04-18 16:26:44 +0800

    test

--- main.py
+++ main.py
@@ -168,102 +168,103 @@ def load_extra_path_config(yaml_path):
 
 
 def run_main(options: Dict[str, Any]):
-    if options.get("output_directory", None):
-        args.output_directory = options["output_directory"]
-    if options.get("input_directory", None):
-        args.input_directory = options["input_directory"]
-    if options.get("temp_directory", None):
-        args.temp_directory = options["temp_directory"]
-    if options.get("server_address", None):
-        args.server_address = options["server_address"]
-    if options.get("client_id", None):
-        args.client_id = options["client_id"]
+    return
+    # if options.get("output_directory", None):
+    #     args.output_directory = options["output_directory"]
+    # if options.get("input_directory", None):
+    #     args.input_directory = options["input_directory"]
+    # if options.get("temp_directory", None):
+    #     args.temp_directory = options["temp_directory"]
+    # if options.get("server_address", None):
+    #     args.server_address = options["server_address"]
+    # if options.get("client_id", None):
+    #     args.client_id = options["client_id"]
 
-    if args.cuda_device is not None:
-        os.environ['CUDA_VISIBLE_DEVICES'] = str(args.cuda_device)
-        logging.info("Set cuda device to: {}".format(args.cuda_device))
+    # if args.cuda_device is not None:
+    #     os.environ['CUDA_VISIBLE_DEVICES'] = str(args.cuda_device)
+    #     logging.info("Set cuda device to: {}".format(args.cuda_device))
 
-    if args.deterministic:
-        if 'CUBLAS_WORKSPACE_CONFIG' not in os.environ:
-            os.environ['CUBLAS_WORKSPACE_CONFIG'] = ":4096:8"
+    # if args.deterministic:
+    #     if 'CUBLAS_WORKSPACE_CONFIG' not in os.environ:
+    #         os.environ['CUBLAS_WORKSPACE_CONFIG'] = ":4096:8"
 
-    import cuda_malloc
-    def cuda_malloc_warning():
-        device = comfy.model_management.get_torch_device()
-        device_name = comfy.model_management.get_torch_device_name(device)
-        cuda_malloc_warning = False
-        if "cudaMallocAsync" in device_name:
-            for b in cuda_malloc.blacklist:
-                if b in device_name:
-                    cuda_malloc_warning = True
-            if cuda_malloc_warning:
-                logging.warning("\nWARNING: this card most likely does not support cuda-malloc, if you get \"CUDA error\" please run ComfyUI with: --disable-cuda-malloc\n")
+    # import cuda_malloc
+    # def cuda_malloc_warning():
+    #     device = comfy.model_management.get_torch_device()
+    #     device_name = comfy.model_management.get_torch_device_name(device)
+    #     cuda_malloc_warning = False
+    #     if "cudaMallocAsync" in device_name:
+    #         for b in cuda_malloc.blacklist:
+    #             if b in device_name:
+    #                 cuda_malloc_warning = True
+    #         if cuda_malloc_warning:
+    #             logging.warning("\nWARNING: this card most likely does not support cuda-malloc, if you get \"CUDA error\" please run ComfyUI with: --disable-cuda-malloc\n")
 
-    if args.temp_directory:
-        temp_dir = os.path.join(os.path.abspath(args.temp_directory), "temp")
-        logging.info(f"Setting temp directory to: {temp_dir}")
-        folder_paths.set_temp_directory(temp_dir)
-    cleanup_temp()
+    # if args.temp_directory:
+    #     temp_dir = os.path.join(os.path.abspath(args.temp_directory), "temp")
+    #     logging.info(f"Setting temp directory to: {temp_dir}")
+    #     folder_paths.set_temp_directory(temp_dir)
+    # cleanup_temp()
 
-    if args.windows_standalone_build:
-        try:
-            import new_updater
-            new_updater.update_windows_updater()
-        except:
-            pass
+    # if args.windows_standalone_build:
+    #     try:
+    #         import new_updater
+    #         new_updater.update_windows_updater()
+    #     except:
+    #         pass
 
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    server = Server.PromptServer(loop)
-    q = execution.PromptQueue(server)
+    # loop = asyncio.new_event_loop()
+    # asyncio.set_event_loop(loop)
+    # server = Server.PromptServer(loop)
+    # q = execution.PromptQueue(server)
 
-    extra_model_paths_config_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "extra_model_paths.yaml")
-    if os.path.isfile(extra_model_paths_config_path):
-        load_extra_path_config(extra_model_paths_config_path)
+    # extra_model_paths_config_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "extra_model_paths.yaml")
+    # if os.path.isfile(extra_model_paths_config_path):
+    #     load_extra_path_config(extra_model_paths_config_path)
 
-    if args.extra_model_paths_config:
-        for config_path in itertools.chain(*args.extra_model_paths_config):
-            load_extra_path_config(config_path)
+    # if args.extra_model_paths_config:
+    #     for config_path in itertools.chain(*args.extra_model_paths_config):
+    #         load_extra_path_config(config_path)
 
-    # init_custom_nodes()
+    # # init_custom_nodes()
 
-    # cuda_malloc_warning()
+    # # cuda_malloc_warning()
 
-    server.add_routes()
-    hijack_progress(server)
+    # server.add_routes()
+    # hijack_progress(server)
 
-    threading.Thread(target=prompt_worker, daemon=True, args=(q, server,)).start()
+    # threading.Thread(target=prompt_worker, daemon=True, args=(q, server,)).start()
 
-    if args.output_directory:
-        output_dir = os.path.abspath(args.output_directory)
-        logging.info(f"Setting output directory to: {output_dir}")
-        folder_paths.set_output_directory(output_dir)
+    # if args.output_directory:
+    #     output_dir = os.path.abspath(args.output_directory)
+    #     logging.info(f"Setting output directory to: {output_dir}")
+    #     folder_paths.set_output_directory(output_dir)
 
-    # These are the default folders that checkpoints, clip and vae models will be saved to when using CheckpointSave, etc.. nodes
-    folder_paths.add_model_folder_path("checkpoints", os.path.join(folder_paths.get_output_directory(), "checkpoints"))
-    folder_paths.add_model_folder_path("clip", os.path.join(folder_paths.get_output_directory(), "clip"))
-    folder_paths.add_model_folder_path("vae", os.path.join(folder_paths.get_output_directory(), "vae"))
+    # # These are the default folders that checkpoints, clip and vae models will be saved to when using CheckpointSave, etc.. nodes
+    # folder_paths.add_model_folder_path("checkpoints", os.path.join(folder_paths.get_output_directory(), "checkpoints"))
+    # folder_paths.add_model_folder_path("clip", os.path.join(folder_paths.get_output_directory(), "clip"))
+    # folder_paths.add_model_folder_path("vae", os.path.join(folder_paths.get_output_directory(), "vae"))
 
-    if args.input_directory:
-        input_dir = os.path.abspath(args.input_directory)
-        logging.info(f"Setting input directory to: {input_dir}")
-        folder_paths.set_input_directory(input_dir)
+    # if args.input_directory:
+    #     input_dir = os.path.abspath(args.input_directory)
+    #     logging.info(f"Setting input directory to: {input_dir}")
+    #     folder_paths.set_input_directory(input_dir)
 
-    if args.quick_test_for_ci:
-        exit(0)
+    # if args.quick_test_for_ci:
+    #     exit(0)
 
-    call_on_start = None
-    if args.auto_launch:
-        def startup_server(address, port):
-            import webbrowser
-            if os.name == 'nt' and address == '0.0.0.0':
-                address = '127.0.0.1'
-            webbrowser.open(f"http://{address}:{port}")
-        call_on_start = startup_server
+    # call_on_start = None
+    # if args.auto_launch:
+    #     def startup_server(address, port):
+    #         import webbrowser
+    #         if os.name == 'nt' and address == '0.0.0.0':
+    #             address = '127.0.0.1'
+    #         webbrowser.open(f"http://{address}:{port}")
+    #     call_on_start = startup_server
 
-    try:
-        loop.run_until_complete(run(server, address=args.listen, port=args.port, verbose=not args.dont_print_server, call_on_start=call_on_start))
-    except KeyboardInterrupt:
-        logging.info("\nStopped server")
+    # try:
+    #     loop.run_until_complete(run(server, address=args.listen, port=args.port, verbose=not args.dont_print_server, call_on_start=call_on_start))
+    # except KeyboardInterrupt:
+    #     logging.info("\nStopped server")
 
-    cleanup_temp()
+    # cleanup_temp()
